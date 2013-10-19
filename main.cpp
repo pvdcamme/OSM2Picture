4f935ef8f509e45424e36e2e7d993bd76e2460f1
pieter <pieter@pieter-KB008AA-B14-a6340-be.(none)>
2013-10-19 17:05:10 +0200

    basic XML parsing

--- main.cpp
+++ main.cpp
@@ -11,7 +11,26 @@
 */
 
 using std::cerr;
+using std::cout;
 using std::endl;
+using std::ifstream;
+
+
+XML_Status parseFile(XML_Parser& parser, 
+		ifstream& input)
+{
+	const size_t BUFFERSIZE = 2048;
+	char buffer[BUFFERSIZE];
+	size_t readAmt = 1; //init value for loop
+
+	while(input.good() && readAmt > 0)
+	{
+		input.read(buffer, BUFFERSIZE);
+		readAmt = input.gcount();
+		XML_Parse(parser, buffer, readAmt, false);
+	}
+	return XML_Parse(parser, buffer, 0, true);
+}
 
 int main(int argc, char** argv)
 {
@@ -24,15 +43,20 @@ int main(int argc, char** argv)
 		return -1;
 	}
 
-	std::ifstream xmlIn(argv[1]);
+	ifstream xmlIn(argv[1]);
 	if(!xmlIn.good())
 	{
 		cerr << "Select bad file" << endl;
 		return -2;
 	}
 
-	XML_Parser xmlEvt =  XML_ParserCreate(NULL);
-	XML_ParserFree(xmlEvt);
+	XML_Parser parser =  XML_ParserCreate(NULL);
+	XML_Status parseResult = parseFile(parser, xmlIn);
+	cout << "Did parse correctly? ";
+	cout << std::boolalpha << (parseResult == XML_STATUS_OK);
+	cout << endl;
+
+	XML_ParserFree(parser);
 
 		
 	return true;
