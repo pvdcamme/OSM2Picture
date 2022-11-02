64f16576292eb14535f1c59a277487002ad12451
pvdcamme <pvdcamme@gmail.com>
2022-11-02 23:55:28 +0000

    Try out HSL colorspace

--- render.py
+++ render.py
@@ -2,6 +2,8 @@ import ctypes
 import sys
 import os
 from PIL import Image
+import numpy
+import colorsys
 
 lib = ctypes.cdll.LoadLibrary(os.path.abspath("OSM2Picture.so"))
 
@@ -11,12 +13,15 @@ result = image_type()
 
 lib.file_to_raster(sys.argv[1].encode("ascii"), result, 1024)
 
+np_array = numpy.array(result).reshape((1024,1024))
+max_val = numpy.max(np_array)
 result_image = Image.new("RGB", (1024,1024))
 
 for w in range(1024):
   for h in range(1024):
     val = result[w + h * 1024]
-    result_image.putpixel((w,h), (0,0, 255 * (val > 0)))
+    r,g,b = colorsys.hsv_to_rgb(val / max_val, val > 0,1)
+    result_image.putpixel((w,h), (int(255 * r), int(255 * g), int( 255 *b)))
 
 result_image.save("result.jpg")    
 
