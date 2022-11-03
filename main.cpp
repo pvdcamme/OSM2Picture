1557d900cfb8912db5d9152f2257b6c8ae286387
pvdcamme <pvdcamme@gmail.com>
2022-11-03 22:19:17 +0000

    Autoformatting

--- main.cpp
+++ main.cpp
@@ -22,34 +22,34 @@ using std::endl;
 /** Counts all nodes, mostly for reporting and testing.
  */
 struct CountHandler: public osmium::handler::Handler {
-  size_t counted;
+    size_t counted;
 
-  CountHandler(): counted(0) {}
+    CountHandler(): counted(0) {}
 
-  void node(const osmium::Node& n) {
-    counted++;
-  }
+    void node(const osmium::Node& n) {
+        counted++;
+    }
 };
 
 /** Collects Positions onto a raster.
  */
 struct ImageHandler: public osmium::handler::Handler {
-  ImageMap map;
+    ImageMap map;
 
-  ImageHandler(size_t width, size_t height): map(width, height) {
-    map.setArea(2.208, 51.649, 6.652, 49.293);
-  }
+    ImageHandler(size_t width, size_t height): map(width, height) {
+        map.setArea(2.208, 51.649, 6.652, 49.293);
+    }
 
-  void node(const osmium::Node& n) {
-    auto location = n.location();
-    auto lat = location.lat();
-    auto lon = location.lon();
-    map.addPoint(lon, lat, 1);
-  }
+    void node(const osmium::Node& n) {
+        auto location = n.location();
+        auto lat = location.lat();
+        auto lon = location.lon();
+        map.addPoint(lon, lat, 1);
+    }
 };
 
 
-int priv_file_to_raster(char* file_name, size_t* result, size_t size) 
+int priv_file_to_raster(char* file_name, size_t* result, size_t size)
 {
     std::string name(file_name);
 
@@ -57,9 +57,9 @@ int priv_file_to_raster(char* file_name, size_t* result, size_t size)
     osmium::io::Reader reader(file_name, osmium::osm_entity_bits::node, osmium::io::read_meta::no);
     osmium::apply(reader, mapper);
     for(size_t w(0); w < size; ++w) {
-      for(size_t h(0); h < size; ++h) {
-        result[w + h * size] = mapper.map.getPoint(w,h);
-      }
+        for(size_t h(0); h < size; ++h) {
+            result[w + h * size] = mapper.map.getPoint(w,h);
+        }
     }
 
     return 1;
@@ -67,11 +67,11 @@ int priv_file_to_raster(char* file_name, size_t* result, size_t size)
 
 
 // For use with Python
-extern "C" { 
-int file_to_raster(char* file_name, size_t* result, size_t size)
-{
-    return priv_file_to_raster(file_name, result, size);
-}
-void visit_tags(char* file_name, char* key, char* selected, bool (*callback)(char*)){
-}
+extern "C" {
+    int file_to_raster(char* file_name, size_t* result, size_t size)
+    {
+        return priv_file_to_raster(file_name, result, size);
+    }
+    void visit_tags(char* file_name, char* key, char* selected, bool (*callback)(char*)) {
+    }
 }
