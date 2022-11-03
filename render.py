5a049787140cda717143ae53cf148fc09b0240da
pvdcamme <pvdcamme@gmail.com>
2022-11-03 00:02:34 +0000

    Autoformatting

--- render.py
+++ render.py
@@ -8,24 +8,25 @@ import colorsys
 
 def build_image(file_name):
     lib = ctypes.cdll.LoadLibrary(os.path.abspath("OSM2Picture.so"))
-    
-    image_type= ctypes.c_size_t * (1024 * 1024)
+
+    image_type = ctypes.c_size_t * (1024 * 1024)
     result = image_type()
-    
+
     lib.file_to_raster(file_name.encode("ascii"), result, 1024)
-    
-    np_array = numpy.array(result).reshape((1024,1024))
+
+    np_array = numpy.array(result).reshape((1024, 1024))
     max_val = numpy.max(np_array)
-    result_image = Image.new("RGB", (1024,1024))
-    
+    result_image = Image.new("RGB", (1024, 1024))
+
     for w in range(1024):
-      for h in range(1024):
-        val = result[w + h * 1024]
-        r,g,b = colorsys.hsv_to_rgb(val / max_val, val > 0,1)
-        result_image.putpixel((w,h), (int(255 * r), int(255 * g), int( 255 *b)))
-    
-    result_image.save("result.jpg")    
-    
+        for h in range(1024):
+            val = result[w + h * 1024]
+            r, g, b = colorsys.hsv_to_rgb(val / max_val, val > 0, 1)
+            result_image.putpixel((w, h),
+                                  (int(255 * r), int(255 * g), int(255 * b)))
+
+    result_image.save("result.jpg")
+
+
 if __name__ == "__main__":
-  build_image(sys.argv[1])
-  
+    build_image(sys.argv[1])
